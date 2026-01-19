776eab441702bb2718bb6e80032a157f65e27a93
Mirza Mohammad Azwad <mirzaazwad8@gmail.com>
2026-01-19 19:17:08 +0600

    jest config updated

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,5 @@ module.exports = {
   roots: ['<rootDir>/tests'],
   testMatch: ['<rootDir>/tests/**/*.test.ts', '<rootDir>/tests/**/*.spec.ts'],
   setupFiles: ['dotenv/config'],
-  setupFilesAfterEnv: ['<rootDir>/tests/Init.ts'],
   testTimeout: 90000000,
 }
